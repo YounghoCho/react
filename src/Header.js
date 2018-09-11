33d5e202b1e704a1c6784284dd35039358c87f3f
조영호/Cho YoungHo <youngho.jo@hmm21.com>
2018-09-11 16:16:10 +0900

    Single Page Application in ReactJS

--- src/Header.js
+++ src/Header.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
 import './Header.css';
 
-//클래스 컴포넌트
 class Header extends Component {
   state = {
     page : ''
   }
-  //SPA 함수
+  //SPA function
   submitPage = (e) => {
+   //turn back menu color to '#eee'
+   const menuColor = '#eee';
+   document.getElementById("styleHomeButton").style.color = menuColor;
+   document.getElementById("styleMenu1").style.color = menuColor;
+   document.getElementById("styleMenu2").style.color = menuColor;
+   document.getElementById("styleMenuLast").style.color = menuColor;
    e.target.style.color = '#D2691E';
-   //state값을 부모로 전달한다음. 넘어온 state값에 따라서 show,hide해보자.
+   //pass 'state' to parent for SPA controll
    this.setState({
     page : e.target.innerHTML
    });
-   //setState가 비동기로 실행되기 때문에 setTimeout을 걸어줘야 정상작동한다.
+   //*******************
+   //*setState is Asnyc*
+   //*******************/
    setTimeout(()=>{
     this.props.handleMainPage(this.state.page);
    },1);
   }
-
-  //컴포넌트 렌더링
+  //render component
   render() {
-    //컴포넌트 반환
+    //return component
     return (
       <div id="styleHeader">
         <span id="styleHomeButton" onClick={this.submitPage}>HOME</span>
-        <span id="styleMenu" onClick={this.submitPage}>{this.props.name}</span>
-        <span id="styleMenu"  onClick={this.submitPage}>{this.props.name2}</span>
+        <span id="styleMenu1" onClick={this.submitPage}>{this.props.name}</span>
+        <span id="styleMenu2" onClick={this.submitPage}>{this.props.name2}</span>
         <span id="styleMenuLast"  onClick={this.submitPage}>{this.props.name3}</span>
       </div>
     );

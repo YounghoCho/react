d48b2f63550946d8222b77092103794ff8cb5ea7
조영호/Cho YoungHo <youngho.jo@hmm21.com>
2018-09-12 16:47:29 +0900

    Use D3JS with ReactJS

--- src/App.js
+++ src/App.js
@@ -1,56 +1,43 @@
+//Why React? : 데이터 변화시 가상 문서를 만들어 변경될 부분만 렌더링할 수 있기때문.
 import React, { Component } from 'react';
 import './App.css';
-//클래스 컴포넌트 호출
 import Header from './Header';
 import Home from './Home';
 import Page1 from './Page1';
 import Page2 from './Page2';
 import Page3 from './Page3';
 
+//FB의 create-react-app에는 Webpack이 내장되어 코드 변경을 감지해 자동 업데이트한다. 
 class App extends Component {
-  //show & hide state
-  //props는 변하지 않는 값이다(immutable)
-  state = {
-    visibleHome : 'true',
-    visiblePage1 : 'false',
-    visiblePage2 : 'false',
-    visiblePage3 : 'false'
+  //show & hide state(props는 변하지 않는 값 - immutable)
+  state = { //visible + Page
+    vHome : 'true', vPage1 : 'false', vPage2 : 'false', vPage3 : 'false'
   }
+
   //Header에서 state값을 전달받는다.
   handleMainPage = (pageName) => {
     if(pageName === 'HOME'){
       this.setState({
-        visibleHome : 'true',
-        visiblePage1 : 'false',
-        visiblePage2 : 'false',
-        visiblePage3 : 'false'
+        vHome : 'true', vPage1 : 'false', vPage2 : 'false', vPage3 : 'false'
       });
     }
     else if(pageName === '데이터 분석'){
       this.setState({
-        visibleHome : 'false',
-        visiblePage1 : 'true',
-        visiblePage2 : 'false',
-        visiblePage3 : 'false'
+        vHome : 'false', vPage1 : 'true', vPage2 : 'false', vPage3 : 'false'
       });
     }else if(pageName === '데이터 분포'){
       this.setState({
-        visibleHome : 'false',
-        visiblePage1 : 'false',
-        visiblePage2 : 'true',
-        visiblePage3 : 'false'
+        vHome : 'false', vPage1 : 'false', vPage2 : 'true', vPage3 : 'false'
       });
     }else if(pageName === '데이터 군집'){
       this.setState({
-        visibleHome : 'false',
-        visiblePage1 : 'false',
-        visiblePage2 : 'false',
-        visiblePage3 : 'true'
+        vHome : 'false', vPage1 : 'false', vPage2 : 'false', vPage3 : 'true'
       });
     }else{
       alert("error at handleMainPage()");
     }
   }
+
   render() {
     return (
       //클래스 컴포넌트 사용
@@ -58,10 +45,10 @@ class App extends Component {
         <Header name='데이터 분석' name2='데이터 분포' name3='데이터 군집'
                 handleMainPage={this.handleMainPage}></Header>
         {/*home*/}
-        {this.state.visibleHome === 'true' ? (<Home></Home>) : ''}
-        {this.state.visiblePage1 === 'true' ? (<Page1></Page1>) : ''}
-        {this.state.visiblePage2 === 'true' ? (<Page2></Page2>) : ''}
-        {this.state.visiblePage3 === 'true' ? (<Page3></Page3>) : ''}
+        {this.state.vHome === 'true' ? (<Home data={[5,10,1,3,24,11,2,4,9]} size={[500,500]}></Home>) : ''}
+        {this.state.vPage1 === 'true' ? (<Page1></Page1>) : ''}
+        {this.state.vPage2 === 'true' ? (<Page2></Page2>) : ''}
+        {this.state.vPage3 === 'true' ? (<Page3></Page3>) : ''}
       </div>
     );
   }
--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,69 @@
-import React, { Component } from 'react';
+//d3 with react ( BarChart )
+//from : https://medium.com/@Elijah_Meeks/interactive-applications-with-react-d3-f76f7b3ebc71
+//ref : https://medium.com/technical-credit/declarative-d3-examples-in-react-6e736e526182
+import React, { Component } from 'react'
+import './App.css'
+import { scaleLinear } from 'd3-scale'
+import { max } from 'd3-array'
+import { select } from 'd3-selection'
 
-class Page1 extends Component {
-  render() {
-    const styleBody = {
-        textAlign : 'center',
-        marginTop : '400px',
-        fontSize : '30px',
-        fontWeight : 'bold',
-        color : '#D2691E'
-    };
-    return (
-        <div style={styleBody}>D3 Analysis Home</div>
-    );
+class Home extends Component {
+   //api_1 : 컴포넌트 생성
+   constructor(props){
+      super(props)
+      //받아온 props를 일반함수에서 사용하기 위해서는 bind해야한다(LifeCycle Func에서는 필요없음)
+      this.createBarChart = this.createBarChart.bind(this)
+   }
+   //api_2 : 컴포넌트 생성 완료 -> re-draw
+   componentDidMount() {
+      this.createBarChart()
+   }
+   //api_3 : prop/state 변화로 리렌더링 완료 -> re-draw
+   componentDidUpdate() {
+      this.createBarChart()
+   }
+   
+   createBarChart() {
+    //react도 DOM을 렌더링하고, D3도 Jquery라서 DOM을 렌더링하려고한다(충돌)
+    //react에서 <svg ref=>를 통해 DOM을 D3에게 전달해서 D3가 렌더링하게한다.
+    const node = this.node
+    //props들은 this.props.data로 접근한다.
+    const dataMax = max(this.props.data)
+    const yScale = scaleLinear()
+        .domain([0, dataMax])
+        .range([0, this.props.size[1]])
+        select(node)
+            .selectAll('rect')
+            .data(this.props.data)
+            .enter()
+            .append('rect')        
+        select(node)
+            .selectAll('rect')
+            .data(this.props.data)
+            .exit()
+            .remove()        
+        select(node)
+            .selectAll('rect')
+            .data(this.props.data)
+            .style('fill', '#fe9922')
+            .attr('x', (d,i) => i * 25)
+            .attr('y', d => this.props.size[1] - yScale(d))
+            .attr('height', d => yScale(d))
+            .attr('width', 25)
+   }
+
+//작업순서 : react rendering -> D3 rendering
+//호출순서 : render()->return()->createBarChart()
+render() {
+  const styleChart={
+     border : '1px solid #333',
+     margin : '10%'
   }
+      return (
+        <svg ref={node => this.node = node}
+        width={1024} height={500} style={styleChart}>
+        </svg>
+      )
+   }
 }
-
-export default Page1;
+export default Home
